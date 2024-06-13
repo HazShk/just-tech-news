d576c3e00d0fb6b07fb0cc0436ee5aaf1b539805
Hassan Shaikh <shaikh.hassan01@outlook.com>
2024-06-13 09:11:20 -0400

    excluded the password atribute

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,10 @@ const { User } = require("../../models");
 //GET /api/users
 router.get("/", (req, res) => {
   //Access our User model and run .findAll() method
-  User.findAll()
+  User.findAll({
+    //excludes password column
+    attributes: { exclude: ["password"] },
+  })
     .then((dbUserData) => res.json(dbUserData))
     .catch((err) => {
       console.log(err);
@@ -15,6 +18,8 @@ router.get("/", (req, res) => {
 //GET /api/users/1
 router.get("/:id", (req, res) => {
   User.findOne({
+    //exclude password column
+    attributes: { exclude: ["password"] },
     where: {
       id: req.params.id,
     },
