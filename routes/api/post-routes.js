ee9e0862a34aac0a03fa743f2e3f5255fbd1666e
Hassan Shaikh <shaikh.hassan01@outlook.com>
2024-06-14 16:05:45 -0400

    updated the GET routes to include votes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -6,7 +6,17 @@ const sequelize = require("../../config/connection");
 router.get("/", (req, res) => {
   console.log("==========================");
   Post.findAll({
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at"[
+        (sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count")
+      ],
+    ],
     order: [["created_at", "DESC"]],
     include: [
       {
@@ -16,6 +26,44 @@ router.get("/", (req, res) => {
     ],
   })
     .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//Get one post
+router.get("/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbPostData);
+    })
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
