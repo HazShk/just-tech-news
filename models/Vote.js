97309a041a7c09f21ab7ff56c245a9c7f770a9c3
Hassan Shaikh <shaikh.hassan01@outlook.com>
2024-06-14 10:50:11 -0400

    created the vote model schema

---
+++ models/Vote.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+//create our Vote Model
+class Vote extends Model {}
+
+//define table columns and configuration
+Vote.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "vote",
+  }
+);
+
+module.exports = Vote;
