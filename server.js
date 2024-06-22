e38f473207602483890191d1b929548fcefc3f2e
Hassan Shaikh <shaikh.hassan01@outlook.com>
2024-06-22 12:48:32 -0400

    setup the template engine

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const routes = require("./routes");
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +15,9 @@ app.use(express.static(path.join(__dirname, "public")));
 
 //turn on routes
 app.use(routes);
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 //turn on connection to db and server
 //when updating the relationships between the tables, we need to use sequelize.sync({ force: true })to drop the tables and recreate them!
